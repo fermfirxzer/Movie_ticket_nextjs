60bfaccc74fda342d9d5c0718c2d229090922699
fermfirxzer <106776702+fermfirxzer@users.noreply.github.com>
2024-10-21 15:10:53 +0700

    testing stripe payment

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -31,7 +31,7 @@ export async function POST(req) {
         console.log('Seat purchase successful for session:', session.id);
       try {
         const origin = req.headers.get('origin');
-        const response = await fetch(`http://localhost:3000/api/seat`, {
+        const response = await fetch(`/api/seat`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -50,7 +50,7 @@ export async function POST(req) {
     }else if (metadata.type === 'tier') {
       console.log('Promotion purchase successful for session:', session.id);
       try {
-        const response = await fetch(`http://localhost:3000/api/promotionTier`, {
+        const response = await fetch(`/api/promotionTier`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
